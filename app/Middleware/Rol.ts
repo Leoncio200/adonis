64527f42cb34b487fc35bfd280585ba2faf33767
Leoncio <leonciopimentelperez@gmail.com>
2023-03-09 00:20:54 -0600

    rol

--- app/Middleware/Rol.ts
+++ app/Middleware/Rol.ts
@@ -2,11 +2,13 @@ import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
 import User from 'App/Models/User'
 export default class Rol {
   public async handle({auth, response}: HttpContextContract, next: () => Promise<void>, allowedRoles: number[]) {
-    const uth = auth.use('api').user
-    const user = await User.find(uth?.id)
-    if (!user || !user.hasRole(...allowedRoles)) {
+    let uth = auth.use('api').user
+    let user = await User.find(uth?.id)
+    if (!user?.hasRole(...allowedRoles)) {
       return response.status(400).send({ error: "No tienes permisos para realizar esta acción"})
     }
+    return response.status(400).send({ error: allowedRoles})
+
     await next()
   }
 }
