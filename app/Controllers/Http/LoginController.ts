0cf24f6e66c3b8354750a022285a8bac167eb52e
eder24m3za <egmr.49@gmail.com>
2023-04-05 15:43:55 -0600

    Borracion
    Lo que el estupido del bryan no pudo hacer

--- app/Controllers/Http/LoginController.ts
+++ app/Controllers/Http/LoginController.ts
@@ -143,21 +143,3 @@ export default class LoginController {
       }
     }
 }
-
-
-//telefono = request.telefono
-//        try {
-//            const apiResponse = await this.axios.post('https://rest.nexmo.com/sms/json', { 
-//                from:"Vonage APIs",
-//                text:`Codigo de verificacion: ${this.numero}`,
-//                to:`52${this.telefono}`,
-//            }, {
-//            auth: { username: 'ddaea82e',
-//            password: 'UiZowFl15b4NhVxb' }
-//            })
-//            response.json({
-//                status:apiResponse.status})
-//        } catch (error) {
-//            response.status(500).send('Error al enviar el SMS')
-//        }
-
--- app/Controllers/Http/PaisController.ts
+++
@@ -1,7 +0,0 @@
-
-export default class PaisController {
-    async SeleccionarPais({ response }) {
-      const paises = await Pais.all()
-      return response.json(paises)
-    }
-  }
